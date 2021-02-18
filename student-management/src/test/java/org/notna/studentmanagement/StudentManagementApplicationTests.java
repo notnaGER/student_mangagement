5f64f78ff3d082b41acd8fad0dc02315d6ba1c28
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-02-18 15:30:00 +0100

    init project

---
+++ student-management/src/test/java/org/notna/studentmanagement/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.notna.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

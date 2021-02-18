6312b778a625000e96e8fcd9be22b307a27f42da
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-02-18 16:13:04 +0100

    delete getStudent from class StudentManagementApplication

--- student-management/src/main/java/org/notna/studentmanagement/StudentManagementApplication.java
+++ student-management/src/main/java/org/notna/studentmanagement/StudentManagementApplication.java
@@ -19,12 +19,4 @@ public class StudentManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-
-	@GetMapping
-	public List<Student> hello(){
-		List<Student> studentList = new ArrayList<Student>();
-		studentList.add(new Student(1L,"Tim", "tim@gmail.com", LocalDate.of(2000, Month.SEPTEMBER, 10), 21));
-		return studentList;
-	}
-
 }

d9f967831d5aeb18035702dedde1829531216dbf
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-02-18 17:56:19 +0100

    add annotations to class Student

--- student-management/src/main/java/org/notna/studentmanagement/student/Student.java
+++ student-management/src/main/java/org/notna/studentmanagement/student/Student.java
@@ -2,7 +2,28 @@ package org.notna.studentmanagement.student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+
+	@Id
+	@SequenceGenerator(
+			name = "student_sequence",
+			sequenceName = "student_sequence",
+			allocationSize =1
+			)
+
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "student_sequence"
+			)
 
 	private Long id;
 	private String name;
@@ -10,8 +31,10 @@ public class Student {
 	private LocalDate dob;
 	private Integer age;
 
+
 	public Student() {
 	}
+
 
 	public Student(String name, String email, LocalDate dob, Integer age) {
 		this.name = name;

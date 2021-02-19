dcad2d3be40f65a67e6e614bc7e45eb229fef81c
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-02-19 11:51:12 +0100

    add delete function from student in server

--- student-management/src/main/java/org/notna/studentmanagement/student/StudentService.java
+++ student-management/src/main/java/org/notna/studentmanagement/student/StudentService.java
@@ -26,5 +26,16 @@ public class StudentService {
 		}
 		studentRepository.save(student);
 	}
+
+	public void deleteStudent(Long studentId) {
+		studentRepository.findById(studentId);
+		boolean exists = studentRepository.existsById(studentId);
+		if(!exists){
+			throw new IllegalStateException(
+					"student with id "+ studentId+" does not exists");
+		}
+		studentRepository.deleteById(studentId);
+
+	}
 }
 

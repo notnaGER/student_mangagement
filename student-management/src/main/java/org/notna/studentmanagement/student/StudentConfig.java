ea0376265c34bd364a88153f682bea9b9abab736
notnaGER <75083505+notnaGER@users.noreply.github.com>
2021-02-19 10:12:23 +0100

    create class StudentConfig

---
+++ student-management/src/main/java/org/notna/studentmanagement/student/StudentConfig.java
@@ -0,0 +1,26 @@
+package org.notna.studentmanagement.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository repository){
+		return args ->{
+			Student tim = new Student("Tim", "tim@gmail.com", LocalDate.of(2000, Month.SEPTEMBER, 10), 21);
+			Student alex = new Student("Alex", "alex@gmail.com", LocalDate.of(2007, Month.SEPTEMBER, 10), 21);
+			repository.saveAll(Arrays.asList(tim,alex));
+		};
+	}
+}
+
+
